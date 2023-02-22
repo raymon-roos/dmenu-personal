898fb31630ffe7b4605785c59af7161efe0c5b60
Raymon Roos <raymon.roos@hotmail.com>
2023-02-22 22:34:01 +0100

    feat(patch): applied highlight patch, use orange colours

--- config.def.h
+++ config.def.h
@@ -4,17 +4,20 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+    "CaskaydiaCove Nerd Font:style=Regular:size=9:antialias=true"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = ">";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel]  = { "#eeeeee", "#ab381c" },
+    [SchemeSelHighlight] = { "#ffc978", "#ab381c" },
+    [SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
+    [SchemeOutHighlight] = { "#ffc978", "#00ffff" }, 
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
