46100c1886036fbda4f2c8520f667f5f779fd97e
Raymon Roos <raymon.roos@hotmail.com>
2023-03-03 22:04:52 +0100

    style: align inline comments and equals signs

--- config.def.h
+++ config.def.h
@@ -1,10 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;          /* -b  option; if 0, dmenu appears at bottom  */
-static int centered = 0;        /* -c option; centers dmenu on screen */
-static int min_width = 550;     /* minimum width when centered */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int min_width = 550;  /* minimum width when centered */
+static int centered  = 0;    /* -c option; centers dmenu on screen */
+static int topbar    = 1;    /* -b  option; if 0, dmenu appears at bottom  */
+static int fuzzy     = 1;    /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -13,10 +13,10 @@ static const char *fonts[] = {
 
 static const char *prompt = ">";
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel]  = { "#eeeeee", "#ab381c" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
+	                      /*     fg         bg     */
+	[SchemeNorm]          = { "#bbbbbb", "#222222" },
+	[SchemeSel]           = { "#eeeeee", "#ab381c" },
+	[SchemeOut]           = { "#000000", "#00ffff" },
     [SchemeNormHighlight] = { "#ffc978", "#222222" },
     [SchemeSelHighlight]  = { "#ffc978", "#ab381c" },
     [SchemeOutHighlight]  = { "#ffc978", "#00ffff" }, 
